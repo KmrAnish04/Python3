4715c73aa85e05117a06a97cbbccd00c287f42d4
KumarAn4 <71969303+KumarAn4@users.noreply.github.com>
2020-10-01 22:05:56 +0530

    Updated fstring.py: Added an example of f-strings.
    Added an example of f-strings in python.

--- fstring.py
+++ fstring.py
@@ -1,3 +1,7 @@
 name = 'Anish'
 age = 17
 my_str =
+val = 'Github'
+
+print(f"{val} is a website whrere you can contribute to open source, {val}.") 
+print(f"Hello, My name is {name} and I'm {age} years old.") 
